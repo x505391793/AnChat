09013c554b2d84bd9c317336ebd5d2adb16f1c38
x505391793 <505391793@qq.com>
2026-07-10 09:38:28 +0800

    fix: settings.gradle.kts 补齐 Gradle 插件版本，否则 Android Studio 无法 Sync

--- AnChat-Android/settings.gradle.kts
+++ AnChat-Android/settings.gradle.kts
@@ -12,6 +12,14 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24" apply false
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
